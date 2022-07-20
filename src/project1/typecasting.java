45adfcf3128dfde9ac71e91b2a7bdefbd33e844d
paluru-naveen <naveenpaluru2000@gmail.com>
2022-07-20 19:00:08 +0530

    practice project1

---
+++ src/project1/typecasting.java
@@ -0,0 +1,18 @@
+package project1;
+
+public class typecasting {
+
+	public static void main(String[] args) {
+	int a=5;
+	float f=(float)a;
+	System.out.println(f);
+	
+	
+	//explict typecasting
+	char c='d';
+	int d=(int)c;
+	System.out.println(d);
+
+	}
+
+}
